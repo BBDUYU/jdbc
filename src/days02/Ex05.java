b85ae93cad9e7516599d26b97c1ab56774864c3d
BBDUYU <tmdwhddh@gmail.com>
2025-11-04 17:39:44 +0900

    jdbc

--- src/days02/Ex05.java
+++ src/days02/Ex05.java
@@ -4,12 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Scanner;
-
-import org.doit.domain.EmpVO;
 
 import com.util.DBConn;
 
